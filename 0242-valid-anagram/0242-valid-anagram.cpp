0a3130cc8579ef9dafae9dc8bbb8d9870362201d
Rajan Singh <shriganesha97177@gmail.com>
2026-08-15 18:03:43 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,15 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         int sl = s.length(), tl = t.length();
         if(sl != tl) return false;
-        vector<int> sfreq(26, 0);
-        vector<int> tfreq(26, 0);
+        vector<int> freq(26, 0);
         for(int i =0; i<sl; i++){
-            sfreq[s[i] - 'a']++;
-            tfreq[t[i] - 'a']++;
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
-        if(sfreq == tfreq) return true;
+        for(int f : freq){
+            if(f != 0) return false;
+        }
 
-        return false;
+        return true;
 
     }
 };
