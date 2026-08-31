11c2d10b2f109a3d5a2d25ade7cd9bcb20518002
Rajan Singh <shriganesha97177@gmail.com>
2026-08-31 16:23:57 +0530

    Time: 8 ms (90.18%), Space: 58.2 MB (60.57%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,15 +10,15 @@
  */
 class Solution {
 public:
-ListNode* arr2LL(vector<int>& arr, int n, ListNode* head){
-    head->val = arr[0];
-    ListNode* current = head->next;
-    for(int i=1; i<n; i++){
-        current->val = arr[i];
-        current = current->next;
-    }
-    return head;
-}
+// ListNode* arr2LL(vector<int>& arr, int n, ListNode* head){
+//     head->val = arr[0];
+//     ListNode* current = head->next;
+//     for(int i=1; i<n; i++){
+//         current->val = arr[i];
+//         current = current->next;
+//     }
+//     return head;
+// }
     ListNode* sortList(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
         vector<int> nums;
@@ -28,16 +28,16 @@ ListNode* arr2LL(vector<int>& arr, int n, ListNode* head){
             temp = temp->next;
         }
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return arr2LL(nums, n, head);
-        // int i=0; 
-        // ListNode* temp2 = head;
-        // while(temp2!=NULL){
-        //     temp2->val = nums[i];
-        //     i++;
-        //     temp2 = temp2->next;
-        // }
-        // return head;
+        // int n = nums.size();
+        // return arr2LL(nums, n, head);
+        int i=0; 
+        ListNode* temp2 = head;
+        while(temp2!=NULL){
+            temp2->val = nums[i];
+            i++;
+            temp2 = temp2->next;
+        }
+        return head;
 
     }
 };
